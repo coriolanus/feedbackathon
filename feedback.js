8f7be333cb5c1f973f662fc036c8b9a06e6446ff
Jani Lahtinen <jani.lahtinen8@gmail.com>
2018-06-28 16:24:08 +0100

    Parameters from page

--- feedback.js
+++ feedback.js
@@ -1,6 +1,16 @@
 window.onload = function() {
-	//alert("Loaded!");
+}
 
+function getParamValue(paramName)
+{
+    var url = window.location.search.substring(1); //get rid of "?" in querystring
+    var qArray = url.split('&'); //get key-value pairs
+    for (var i = 0; i < qArray.length; i++)
+    {
+        var pArr = qArray[i].split('='); //split key and value
+        if (pArr[0] == paramName)
+            return pArr[1]; //return value
+    }
 }
 
 function send_feedback() {
@@ -8,18 +18,22 @@ function send_feedback() {
 	var env = get_environment_details();
 	var form = get_form_details();
 
-	var subject = "Feedback on APPNAME";
+	var title = getParamValue('title')
+	var email = getParamValue('email')
+
+	var subject = "Feedback on " + title;
 	var content = env + form;
-	
+
 	// Step 1: Craft link
-	var href = "mailto://" + contact + "?subject=" + subject + "&body=" + content;
-	
+	//var href = "mailto://" + contact + "?subject=" + subject + "&body=" + content;
+
 	// Step 2: Click it!
 }
 
 function get_environment_details() {
 	return {
 		"userAgent": navigator.userAgent,
+		"href": navigator.href,
 		"timestamp": new Date()
 	}
 }
